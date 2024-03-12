096cc33e7051c7172134293bc5bc9bc030e80a35
briansilah <unpervertedkid@gmail.com>
2024-03-12 17:20:18 +0300

    feat: Add analytics support

--- app/layout.tsx
+++ app/layout.tsx
@@ -4,6 +4,7 @@ import "./globals.css";
 import { Toaster } from "@/components/ui/toaster";
 import Navbar from "@/components/ui/navbar";
 import { ThemeProvider } from "@/components/theme-provider";
+import { Analytics } from '@vercel/analytics/react';
 
 const inter = Inter({ subsets: ["latin"] });
 
@@ -29,6 +30,7 @@ export default function RootLayout({
           <Navbar />
           <main>{children}</main>
           <Toaster />
+          <Analytics />
         </ThemeProvider>
       </body>
     </html>
