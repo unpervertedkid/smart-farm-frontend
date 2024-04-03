b62cb6ebf2896cd78941496aeb93459ced472fe7
briansilah <unpervertedkid@gmail.com>
2024-04-03 07:00:43 +0300

    fix: some magic to make it not static??

--- app/api/get-analytics/route.ts
+++ app/api/get-analytics/route.ts
@@ -1,11 +1,15 @@
-export const fetchCache = "force-no-store";
 import { sql } from "@vercel/postgres";
 import { NextResponse } from "next/server";
 
 export async function GET(request: Request) {
   try {
-    const analytics = await sql`SELECT * FROM Analytics;`;
-    return NextResponse.json({ analytics }, { status: 200 });
+    let isanalyticsrequest = request.url.includes("analytics");
+    console.log(`isanalyticsrequest: ${isanalyticsrequest}`);
+    if (!isanalyticsrequest) throw new Error("Invalid request");
+    else {
+      const analytics = await sql`SELECT * FROM Analytics;`;
+      return NextResponse.json({ analytics }, { status: 200 });
+    }
   } catch (error) {
     return NextResponse.json({ error }, { status: 500 });
   }
